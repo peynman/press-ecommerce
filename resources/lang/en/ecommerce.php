f69447df1e158d5b7fa0d9d394b8745b33378774
peyman <lord.akinak@gmail.com>
2020-06-25 02:46:45 +0430

    lang

--- resources/lang/en/ecommerce.php
+++ resources/lang/en/ecommerce.php
@@ -5,5 +5,9 @@ return [
         'messages' => [
             'bank-forwared' => 'request for cart id :cart_id for amount :amount :currency',
         ]
+    ],
+
+    'messaging' => [
+        'sms_send_error' => 'خطایی در ارسال پیامک بوجود آمد، لطفا بعدا تلاش کنید '
     ]
 ];
