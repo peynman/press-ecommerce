fe2a708986ef4bf61f95c9da22eed5e2b1fba8e1
peyman <lord.akinak@gmail.com>
2020-12-14 17:59:00 +0330

    add file service cache header

--- src/Services/FileUpload/FileUploadService.php
+++ src/Services/FileUpload/FileUploadService.php
@@ -204,6 +204,7 @@ class FileUploadService implements IFileUploadService
             }
         }, 200, [
             'Content-Type' => $link->mime,
+            'Cache-Control' => 'max_age=592200, public'
         ]);
     }
 
